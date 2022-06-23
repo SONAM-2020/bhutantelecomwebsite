b7afcea156c24425e94922cc20476515976690ac
SONAM DORJI <sdorji815@gmail.com>
2022-06-23 12:24:42 +0600

    changes

--- application/controllers/WebsiteController.php
+++ application/controllers/WebsiteController.php
@@ -5,5 +5,12 @@ class WebsiteController extends CI_Controller {
 	public function index(){
         $this->load->view('web/index',$page_data);
 	}
+    function loadpage($param1="",$param2=""){
+        if($param1=="login"){
+            $page_data['linktype']=$param1;
+            $this->load->view('web/pages/login', $page_data);   
+        }
+       
+    }
 }
 
--- application/views/web/includes/head.php
+++ application/views/web/includes/head.php
@@ -2,43 +2,26 @@
 <html class="no-js" lang="zxx">
 <?php header('Access-Control-Allow-Origin: *'); ?>
 <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Home | Bhutan Telecom Ltd</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
-        <!-- Material Design Iconic Font-V2.2.0 -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/material-design-iconic-font.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/font-awesome.min.css">
-        <!-- Font Awesome Stars-->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/fontawesome-stars.css">
-        <!-- Meanmenu CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/meanmenu.css">
-        <!-- owl carousel CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/owl.carousel.min.css">
-        <!-- Slick Carousel CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/slick.css">
-        <!-- Animate CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/animate.css">
-        <!-- Jquery-ui CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/jquery-ui.min.css">
-        <!-- Venobox CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/venobox.css">
-        <!-- Nice Select CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/nice-select.css">
-        <!-- Magnific Popup CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/magnific-popup.css">
-        <!-- Bootstrap V4.1.3 Fremwork CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/bootstrap.min.css">
-        <!-- Helper CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/helper.css">
-        <!-- Main Style CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/style.css">
-        <!-- Responsive CSS -->
-        <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/responsive.css">
-        <!-- Modernizr js -->
-        <script src="<?php echo base_url();?>/assest/web/js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Home | Bhutan Telecom Ltd</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>/assest/web/images/favicon.png">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/fontawesome-stars.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/meanmenu.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/slick.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/animate.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/jquery-ui.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/venobox.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/nice-select.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/helper.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/style.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assest/web/css/responsive.css">
+    <script src="<?php echo base_url();?>/assest/web/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
